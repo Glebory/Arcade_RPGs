1b7e87a9c3ea3c4fad5283b4fbcac9ce4b7cf39d
peadar-oconnor <89516373+peadar-oconnor@users.noreply.github.com>
2022-02-07 12:13:58 +0000

    split health and mana into current and total, added methods to manipulate certain variables

--- Text-Adventure/character.py
+++ Text-Adventure/character.py
@@ -1,5 +1,6 @@
 import inventory
 
+
 class Character:
     """
     Class representing characters, parent class to players and enemies. Maybe npc's
@@ -10,27 +11,42 @@ class Character:
     def __init__(self, name, level, health):
         self._name = name
         self._level = level
-        self._health = health
+        self._total_health = health
+        self._current_health = self._total_health
         self._strength = self._level * 1.5
-        self._mana = self._level*5
+        self._total_mana = self._level * 5
+        self._current_mana = self._total_mana
         self._resistances = []
         self._weaknesses = []
         self._image = "image"
-    
+
     def __str__(self):
         return self._name
 
-    def set_health(self, amount):
-        self._health = amount
-    
-    def get_health(self):
-        return self._health
-    
+    # keeps current health within bounds of total health
+    def health_check(self):
+        if self._current_health > self._total_health:
+            self._current_health = self._total_health
+
+    def set_total_health(self, amount):
+        self._total_health = amount
+
+    def get_total_health(self):
+        return self._total_health
+
     def set_current_health(self, amount):
-        self._health = amount
-    
-    def set_current_health(self):
-        return self._health
+        self._current_health = amount
+        self.health_check()
+
+    def get_current_health(self):
+        return self._current_health
+
+    def take_damage(self, amount):
+        self._current_health -= amount
+
+    def take_healing(self, amount):
+        self._current_health += amount
+        self.health_check()
 
     def get_strength(self):
         return self._strength
@@ -38,36 +54,62 @@ class Character:
     def set_strength(self, amount):
         self._strength = amount
 
-    def get_mana(self):
-        return self._mana
-    
-    def set_mana(self, amount):
-        self._mana = amount
-    
+    # keeps current mana within bounds of total mana
+    def mana_check(self):
+        if self._current_mana > self._total_mana:
+            self._current_mana = self._total_mana
+        elif self._current_mana < 0:
+            self._current_mana = 0
+
+    def get_total_mana(self):
+        return self._total_mana
+
+    def set_total_mana(self, amount):
+        self._total_mana = amount
+
     def get_current_mana(self):
         return self._current_mana
-    
+
     def set_current_mana(self, amount):
-        self._current_mana = amount 
+        self._current_mana = amount
+        self.mana_check()
+
+    def use_mana(self, amount):
+        self._current_mana -= amount
+        self.mana_check()
+
+    def gain_mana(self, amount):
+        self._current_mana += amount
+        self.mana_check()
 
     def get_resistances(self):
         return self._resistances
-    
-    def set_resistances(self, resistances):
-        self._resistances = resistances
-    
+
+    def give_resistance(self, resistance):
+        if resistance not in self._resistances:
+            self._resistances.append(resistance)
+
+    def remove_resistance(self, resistance):
+        if resistance in self._resistances:
+            self._resistances.remove(resistance)
+
     def get_weaknesses(self):
         return self._weaknesses
-    
-    def set_weaknesses(self, weaknesses):
-        self._weaknesses = weaknesses
+
+    def give_weakness(self, weakness):
+        if weakness not in self._weaknesses:
+            self._weaknesses.append(weakness)
+
+    def remove_weakness(self, weakness):
+        if weakness in self._weaknesses:
+            self._weaknesses.remove(weakness)
+
 
 class Player_swordsman(Character):
-    #either the only player option or one of many, bard, mage, etc
+    # either the only player option or one of many, bard, mage, etc
     def __init__(self, name, level, health, resistances=[]):
         super().__init__(name, level, health)
         self._resistances = ["Melee"]
-
 
 
 knight1 = Player_swordsman("Bertrand", 1, 10)
